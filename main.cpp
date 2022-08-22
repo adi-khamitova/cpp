2d6bfd5f20e9e90b870e749909b34681ceb34ec7
Adelya Khamitova <adi.khamitova@gmail.com>
2022-08-22 17:04:44 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+// возвращаемыеданные имяфункции(параметры)
+
+int main(int argc, char** argv) {
+    printf("argc value: %d, argv[0]: %s, argv[1]: %s\n", argc, argv[0], argv[1]);
+    return 0;
+}
