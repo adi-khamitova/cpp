6bdfccdf55556bced65c250a5a42133ba2e1b5bb
Adelya Khamitova <adi.khamitova@gmail.com>
2022-08-22 21:20:36 +0000

    did it

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using std::cout;
+
+int sum2(int a, int b) {
+    return a + b;
+}
+
+
+void sum_all() {
+    int v = 1;
+    int s = 0;
+    for (int i = 0; i < 9; i++) {
+        s = sum2(s, v);
+        v++;
+    }
+    cout << s << "\n";
+} 
+
+
+int main() {
+    sum_all();
+}
