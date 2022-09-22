ef89c87d023a0a6719dc4e1826f0d5f7c9e9fc42
Adelya Khamitova <adi.khamitova@gmail.com>
2022-09-22 17:56:01 +0000

    insert/delete

--- lists_task.cpp
+++ lists_task.cpp
@@ -52,11 +52,62 @@ void free(Item* list) {
     }
 }
 
+Item* insert_el(Item* list, int new_data, int pos) {
+
+    Item *crnt;
+    crnt = list;
+    Item *new_el = new Item();
+    new_el -> data = new_data;
+    new_el -> next = nullptr;
+    for (int i = 0; i < pos - 1; i++) {
+        crnt = crnt -> next;
+    }
+    if (crnt != nullptr) {
+        new_el -> next = crnt -> next;
+        crnt -> next = new_el;
+
+    }
+    
+    return list;
+}
+
+Item* delete_el(Item* list, int pos) {
+
+    Item* crnt = list;
+    Item* prev;
+    if (pos != 1) {
+        for (int i = 0; i < pos - 1; i++) {
+            prev = crnt;
+            crnt = crnt -> next;
+        }
+        prev -> next = crnt -> next;
+        delete (crnt);
+        return list;
+    }
+    else {
+        crnt = list -> next;
+        delete(list);
+        return crnt;
+    }
+
+
+}
+
+
 int main(int argc, char** argv) {
 
     srand(time(NULL));
-    Item* list = create(1 + rand()%21);
+    int count = 1 + rand()%21;
+
+    Item* list = create(count);
     print(list);
+
+    insert_el(list, rand(), 1 + rand()%count);
+    print(list);
+
+    list = delete_el(list, 1 + rand()%(1 +count));
+    print(list);
+    
     free(list);
 
 }
