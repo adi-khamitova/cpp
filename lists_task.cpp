05dc678d4ff254b89a3a5bcb13f5e5fee875253e
Adelya Khamitova <adi.khamitova@gmail.com>
2022-09-23 08:01:39 +0000

    include/delete_new

--- lists_task.cpp
+++ lists_task.cpp
@@ -59,35 +59,76 @@ Item* insert_el(Item* list, int new_data, int pos) {
     Item *new_el = new Item();
     new_el -> data = new_data;
     new_el -> next = nullptr;
-    for (int i = 0; i < pos - 1; i++) {
-        crnt = crnt -> next;
-    }
-    if (crnt != nullptr) {
-        new_el -> next = crnt -> next;
-        crnt -> next = new_el;
+    int count = 0;
 
+    if (pos != 0) {
+
+        while (crnt != nullptr) {
+            crnt = crnt -> next;
+            count += 1;
+        }
+        count += 1;
+        crnt = list;
+
+
+        if (pos > count + 1) {
+            delete(new_el);
+            return list;
+
+        }
+        else {
+            for (int i = 0; i < pos - 2; i++) {
+                crnt = crnt -> next;
+            }
+            if (crnt != nullptr) {
+                new_el -> next = crnt -> next;
+                crnt -> next = new_el;
+
+            }
+            else {
+                crnt -> next = new_el;
+            }
+            return list;
+        }
     }
     
-    return list;
+    else {
+        new_el -> next = list;
+        return new_el;
+    }
+    
 }
 
 Item* delete_el(Item* list, int pos) {
 
     Item* crnt = list;
     Item* prev;
-    if (pos != 1) {
-        for (int i = 0; i < pos - 1; i++) {
-            prev = crnt;
+    int count = 0;
+    while (crnt != nullptr) {
             crnt = crnt -> next;
+            count += 1;
         }
-        prev -> next = crnt -> next;
-        delete (crnt);
+    count += 1;
+    crnt = list;
+
+    if (pos > count) {
         return list;
     }
     else {
-        crnt = list -> next;
-        delete(list);
-        return crnt;
+        if (pos != 1) {
+            for (int i = 0; i < pos - 1; i++) {
+                prev = crnt;
+                crnt = crnt -> next;
+            }
+            prev -> next = crnt -> next;
+            delete (crnt);
+            return list;
+        }
+        else {
+            crnt = list -> next;
+            delete(list);
+            return crnt;
+        }
     }
 
 
@@ -99,15 +140,15 @@ int main(int argc, char** argv) {
     srand(time(NULL));
     int count = 1 + rand()%21;
 
-    Item* list = create(count);
+    Item* list = create(1 + rand()%21);
     print(list);
 
-    insert_el(list, rand(), 1 + rand()%count);
+    list = insert_el(list, rand(), 1 + rand()%21);
     print(list);
 
-    list = delete_el(list, 1 + rand()%(1 +count));
+    list = delete_el(list, 1 + rand()%21);
     print(list);
-    
+
     free(list);
 
 }
