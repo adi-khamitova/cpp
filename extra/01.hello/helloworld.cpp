f09de20c7633d20e8467aa7260a4d9c9d747f79e
Adelya Khamitova <adi.khamitova@gmail.com>
2022-10-26 16:33:49 +0000

    last

--- extra/01.hello/helloworld.cpp
+++ extra/01.hello/helloworld.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
 // возвращаемыеданные имяфункции(параметры)
 
-
 int main(int argc, char** argv) {
     printf("argc value: %d, argv[0]: %s, argv[1]: %s\n", argc, argv[0], argv[1]);
     return 0;
