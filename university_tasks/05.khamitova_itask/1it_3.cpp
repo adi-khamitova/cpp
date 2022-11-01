09eb71ca57adc0f26815204c1bbd8afd5c1bb0cb
Adelya Khamitova <adi.khamitova@gmail.com>
2022-11-01 15:00:25 +0000

    integral

--- university_tasks/05.khamitova_itask/1it_3.cpp
+++ university_tasks/05.khamitova_itask/1it_3.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-#include <cmath>
+#include <math.h>
+#include <cstdlib>
 #define _USE_MATH_DEFINES
 using namespace std;
+
+double function(double x) {
+    return pow(M_E, x*x) / pow(x, 2);
+}
+
+double derivative2(double x) {
+    return 2*pow(M_E, x*x)*(2 - 3./pow(x,2) + 3./pow(x,4));
+}
+
 int main(int argc, char** argv) {
-    
+    double a = 1;
+    double b = 2;
+    double e = pow(10, -2);
+
+    // 1) find n - number of segments:
+    double n;
+    n = sqrt((max(derivative2(a), derivative2(b)) * pow(b-a, 3)) / (24 * e));
+    n = floor(n);
+
+    // 2) find h - step:
+    double h = (b-a) / n;
+
+
+    // 3) find f:
+    double f;
+    for (int i = 1; i <= n; i++) {
+        f += h* function(a + h/2 + (i-1)*h);
+    }
+
+    cout << "the function value: " << f << endl;
+
+    return 0;
 }
