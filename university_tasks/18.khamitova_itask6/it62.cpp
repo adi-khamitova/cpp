a9f95a303c5da678619478b1befe1f5d1af2c2c9
Adelya Khamitova <adi.khamitova@gmail.com>
2022-12-20 18:20:04 +0000

    author checking

--- university_tasks/18.khamitova_itask6/it62.cpp
+++ university_tasks/18.khamitova_itask6/it62.cpp
@@ -10,6 +10,8 @@
 #include <cstdlib>
 #include <sstream>
 #include <vector>
+#include <cstring>
+
 
 struct Book {
     std::string author;
@@ -20,8 +22,10 @@ struct Book {
 };
 
 
-void read_file(std::vector<Book> &lib) {
-    std::ifstream fin("book2.txt");
+bool read_file(std::vector<Book> &lib, char* file_name) {
+    std::ifstream fin(file_name);
+    if (!(fin.is_open()))
+        return false;
     std::string line;
     bool last_valid = true;
 
@@ -43,9 +47,10 @@ void read_file(std::vector<Book> &lib) {
     }
     if (!last_valid)
         lib.pop_back();
+    return true;
 }
 
-int author_count(std::vector <Book> lib, std::string name) {
+int author_count(std::vector<Book> lib, char* name) {
     int count = 0;
     for (int i = 0; i < lib.size(); i++) {
         if (lib[i].author == name)
@@ -54,20 +59,70 @@ int author_count(std::vector <Book> lib, std::string name) {
     return count;
 }
 
+std::string ToLower(std::string str) {
+    for (int i = 0; i < str.size(); i++) {
+        if (isupper(str[i]))
+            str[i] = tolower(str[i]);
+    }
+    return str;
+}
+
+char* ToLower(char* name) {
+    for (int i = 0; i < strlen(name); i++) {
+        if (isalpha(name[i]))
+            name[i] = tolower(name[i]);
+    }
+    return name;
+}
+
+
+bool author_check(std::vector<Book> lib, char* name) {
+    char *name_low = ToLower(name);
+    for (int i = 0; i < lib.size(); i++) {
+        if (ToLower(lib[i].author).find(name_low, i) != std::string::npos) {
+            strcpy(name, lib[i].author.c_str());
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+bool input_check(int argc, char** argv) {
+    if (argc == 3)
+        return true;
+    else {
+        std::cout << "Usage:" << std:: endl;
+        std::cout << argv[0] << " - programm for books counting by author" << std:: endl;
+        std::cout << argv[0] << " <file> <author> " << std:: endl;
+        std::cout << "<file> - filename for filtering" << std::endl;
+        std::cout << "<author> - name of the author" << std::endl;
+        return false;
+        }
+}
+
+
+
 
 int main(int argc, char** argv) {
-    std::vector <Book> lib;
-    read_file(lib);
 
-    std::string name;
-    std::cout << "enter the author: ";
-    std::getline(std::cin, name);
+    if (!(input_check(argc, argv)))
+        return 0;
 
-    int count = author_count(lib,name);
-    if (count == 0)
-        std::cout << "no books of this author in the lib" << std::endl;
-    else
-        std::cout << count << " books of this author in the lib" << std::endl;
+    std::vector<Book> lib;
+    if (!(read_file(lib, argv[1]))) {
+        std::cout << "failed to open file " << argv[1] << std::endl;
+        return 0;
+    }
+
+    if (!(author_check(lib, argv[2]))) {
+        std::cout << "no books of " << argv[2] << " in the lib" << std::endl;
+        return 0;
+    }
+
+    int count = author_count(lib,argv[2]);
+    std::cout << count << " books of " << argv[2] << " in the lib" << std::endl;
 
     return 0;
 }
